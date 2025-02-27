b22fc27774f3a60033bc6c50d6bc7e74e7976368
hienmoi <nguyenhien1281997@gmail.com>
2025-02-28 00:16:14 +0700

    Edit Demo Class 2

--- src/Demo.java
+++ src/Demo.java
@@ -6,5 +6,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Tester");
         System.out.println("Xin chao");
+        System.out.println("VN");
     }
 }
