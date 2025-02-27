7c381aa5b5b0dbc6a7c31bfea006475db63d8359
hienmoi <nguyenhien1281997@gmail.com>
2025-02-28 00:13:51 +0700

    Edit Demo class

--- src/Demo.java
+++ src/Demo.java
@@ -5,5 +5,6 @@ import java.util.SortedMap;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Tester");
+        System.out.println("Xin chao");
     }
 }
